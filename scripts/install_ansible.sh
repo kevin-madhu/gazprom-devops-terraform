b06df930e136df5ad0a88173486c4359b924a8d8
Kevin Madhu <kevin.madhu@gmail.com>
2023-02-19 10:49:08 +0300

    Modifies script to populate ansible hosts file.

--- scripts/install_ansible.sh
+++ scripts/install_ansible.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 sudo yum update -y
 sudo amazon-linux-extras install ansible2 -y
+
+echo -e "\n[ANSIBLE]" >> /etc/ansible/hosts
+echo ${self.private_ip} >> /etc/ansible/hosts
+
+echo -e "\n[JENKINS]" >> /etc/ansible/hosts
+echo ${aws_instance.jenkins.private_ip} >> /etc/ansible/hosts
+
+echo -e "\n[KUBERNETES]" >> /etc/ansible/hosts
+echo ${aws_instance.kub.private_ip} >> /etc/ansible/hosts
