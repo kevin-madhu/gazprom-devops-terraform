f724d3aaf821dbe79309e0f1285e5a0c9de2792b
Kevin Madhu <kevin.madhu@gmail.com>
2023-02-20 05:07:06 +0300

    Major bugfixes.

--- finava-ansible.tf
+++ finava-ansible.tf
@@ -51,14 +51,14 @@ resource "aws_instance" "ansible" {
     sudo amazon-linux-extras install ansible2 -y
     sudo yum install git -y
 
-    echo -e "\n[ansible]" >> /etc/ansible/hosts
-    echo localhost >> /etc/ansible/hosts
+    sudo echo -e "\n[ansible]" >> /etc/ansible/hosts
+    sudo echo localhost >> /etc/ansible/hosts
 
-    echo -e "\n[jenkins]" >> /etc/ansible/hosts
-    echo ${aws_instance.jenkins.private_ip} >> /etc/ansible/hosts
+    sudo echo -e "\n[jenkins]" >> /etc/ansible/hosts
+    sudo echo ${aws_instance.jenkins.private_ip} >> /etc/ansible/hosts
 
-    echo -e "\n[knodes]" >> /etc/ansible/hosts
-    echo ${aws_instance.kube.private_ip} >> /etc/ansible/hosts
+    sudo echo -e "\n[knodes]" >> /etc/ansible/hosts
+    sudo echo ${aws_instance.kube.private_ip} >> /etc/ansible/hosts
 
     git clone git@github.com:kevin-madhu/gazprom-devops-ansible.git ~/gazprom-devops-ansible
     ansible-playbook ~/gazprom-devops-ansible/setup-cluster.yml
